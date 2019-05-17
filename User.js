05f01bdce28937167dd19fbe2a84dbee11ffaf66
jsrub <48402238+jsrub@users.noreply.github.com>
2019-05-17 14:44:48 +0530

    Add files via upload

---
+++ User.js
@@ -0,0 +1,58 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+UserSchema.pre('save', function (next) {
+    const user = this
+
+    bcrypt.hash(user.password, 10, function (error, encrypted) {
+        user.password = encrypted
+        next()
+    })
+})
+
+module.exports = mongoose.model('User', UserSchema)
